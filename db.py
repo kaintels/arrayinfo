d3d282e5af71b04761c6b6030687cb001de8d48a
Seungwoo <swoohan@kakao.com>
2021-07-04 15:38:58 +0900

    Update db.py

--- db.py
+++ db.py
@@ -5,33 +5,33 @@ con = sqlite3.connect("./database/arrayinfo.db")
 cursor = con.cursor()
 check_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 
-def create_new_table(framework_name):
+def create_new_table(library_name):
     # 테이블-필드 생성
     try:
-        query = "CREATE TABLE "+str(framework_name)+"(id integer primary key autoincrement, function_name text, function_info text);"
+        query = "CREATE TABLE "+str(library_name)+"(id integer primary key autoincrement, function_name text, function_info text);"
         print("created new table. ")
         print("-"* 100)
-        print("name : {0}, created_date : {1}".format(framework_name, check_time))
+        print("name : {0}, created_date : {1}".format(library_name, check_time))
         print("query : ", query)
         cursor.execute(query)
         con.commit()
     except:
         pass
 
-def create_new_record(framework_name, idx, function_name, function_info):
+def create_new_record(library_name, idx, function_name, function_info):
     # 레코드 생성
     try:
-        query = "INSERT INTO "+str(framework_name)+" VALUES("+str(idx)+" , "+str(function_name)+" , "+str(function_info)+");"
+        query = "INSERT INTO "+str(library_name)+" VALUES("+str(idx)+" , "+str(function_name)+" , "+str(function_info)+");"
         print("query : ", query)
         cursor.execute(query)
         con.commit()
     except:
         pass
 
-def read_record(framework_name):
+def read_record(library_name):
     # 레코드 조회
     try:
-        query = "SELECT * FROM "+str(framework_name)+";"
+        query = "SELECT * FROM "+str(library_name)+";"
         print("query : ", query)
         print("\t")
         print("data")
@@ -51,17 +51,17 @@ def update_record():
 def delete_table():
     pass
 
-def delete_record(framework_name, idx, choice_all=None):
+def delete_record(library_name, idx, choice_all=None):
     # 레코드 삭제
     if choice_all is not None:
         print("all data deleted...")
         print("-" * 100)
-        query = "DELETE FROM "+str(framework_name)+";"
+        query = "DELETE FROM "+str(library_name)+";"
         print("query : ", query)
         cursor.execute(query)
         con.commit()
     else:
-        query = "DELETE FROM "+str(framework_name)+" where id="+str(idx)+";"
+        query = "DELETE FROM "+str(library_name)+" where id="+str(idx)+";"
         print("query : ", query)
         cursor.execute(query)
         con.commit()
