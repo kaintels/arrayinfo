48400893b2e1300950daa690bebcda96dc76a199
Seungwoo Han <swoohan@kakao.com>
2021-12-26 17:52:18 +0900

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,10 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+html_baseurl = "https://kaintels.github.io/arrayinfo/"
 extensions = [
+    "sphinx.ext.githubpages",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
